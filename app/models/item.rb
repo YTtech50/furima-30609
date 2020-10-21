d2b8f0596721a64cb16e372f9ad2228b5a5f1777
YTtech50 <y-taka@sa3.so-net.ne.jp>
2020-10-21 22:03:41 +0900

    バリデーションの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -15,4 +15,6 @@ class Item < ApplicationRecord
   validates :item_shipping_fee_status_id, numericality: { other_than: 1 } 
   validates :item_prefecture_id, numericality: { other_than: 1 } 
   validates :item_scheduled_delivery_id, numericality: { other_than: 1 } 
+  validates :item_price, numericality: { greater_than: 300, less_than: 9999999 }
+  validates :item_price, format: { with: /\A[0-9]+\z/ }
 end
