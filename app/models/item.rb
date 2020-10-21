846b86c105cece3e5d828471ceb441d28be62e3d
YTtech50 <y-taka@sa3.so-net.ne.jp>
2020-10-21 14:52:32 +0900

    itemsテーブルのカラムの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,18 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  has_one_attached :image
+  
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :item_category
+  belongs_to_active_hash :item_sales_status
+  belongs_to_active_hash :item_shipping_fee_status
+  belongs_to_active_hash :item_prefecture
+  belongs_to_active_hash :item_scheduled_delivery
+
+  validates :item_name, :item_info, :item_category, :item_sales_status, :item_shipping_fee_status, :item_prefecture, :item_scheduled_delivery, :item_price, :image, presence: true
+  validates :item_category_id, numericality: { other_than: 1 } 
+  validates :item_sales_status_id, numericality: { other_than: 1 } 
+  validates :item_shipping_fee_status_id, numericality: { other_than: 1 } 
+  validates :item_prefecture_id, numericality: { other_than: 1 } 
+  validates :item_scheduled_delivery_id, numericality: { other_than: 1 } 
 end
