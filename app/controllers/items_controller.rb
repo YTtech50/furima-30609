0e8f064d1de628db86d9a8a8fe89134f22784d20
YTtech50 <y-taka@sa3.so-net.ne.jp>
2020-10-24 14:41:26 +0900

    itemsコントローラーの修正・整理

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :destroy]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -39,8 +39,8 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    item = Item.find(params[:id])
-    if item.destroy
+    if @item.user == current_user
+      @item.destroy
       redirect_to root_path
     end
   end
